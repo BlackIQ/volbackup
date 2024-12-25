31ef21423becbd9a33be179d969bdc9fefc09109
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2024-12-25 22:11:44 +0330

    Wow

---
+++ config/config.py
@@ -0,0 +1,8 @@
+minio_config = {
+    'AWS_ENDPOINT_URL': 'http://10.1.1.58:50000',
+    'AWS_ACCESS_KEY': 'spGDo12SwuoBh3pVv7ji',
+    'AWS_SECRET_KEY': 'qah56VYunTMo6LIoFQGEJzEDvqyghiCNE5Ovr9kW',
+    'AWS_REGION_NAME': 'us-east-1',
+    'AWS_BUCkET': 'test-backup',
+    'AWS_USE_SSL': False
+}
