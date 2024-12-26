a860686d3fe8f017a8b931bec6ffb58e158ba20f
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2024-12-26 19:47:48 +0330

    Fuck

--- config/config.py
+++ config/config.py
@@ -15,12 +15,3 @@ minio_config = {
     'AWS_BUCkET': os.getenv("AWS_BUCkET"),
     'AWS_USE_SSL': str_to_bool(os.getenv("AWS_USE_SSL", "false"))
 }
-
-# minio_config = {
-#     'AWS_ENDPOINT_URL': 'http://79.127.87.84:50000',
-#     'AWS_ACCESS_KEY': 'spGDo12SwuoBh3pVv7ji',
-#     'AWS_SECRET_KEY': 'qah56VYunTMo6LIoFQGEJzEDvqyghiCNE5Ovr9kW',
-#     'AWS_REGION_NAME': 'us-east-1',
-#     'AWS_BUCkET': 'backup',
-#     'AWS_USE_SSL': False
-# }
